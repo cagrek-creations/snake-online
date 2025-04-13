713f5bc6221b7b0942260c3022f1214661a653a8
darclander <lukedarclander@gmail.com>
2025-04-13 18:23:01 +0200

    add the possibility to create effects

---
+++ src/headers/Effect.hpp
@@ -0,0 +1,33 @@
+#pragma once
+
+class Effect {
+
+    public:
+        Effect(float duration) : m_duration(duration), m_elapsed(0.0f), m_active(true), m_applied(false) {}
+        virtual ~Effect() = default;
+
+        bool isActive() const { return m_active; }
+
+        virtual void apply() = 0;
+        virtual void expire() = 0;
+        virtual void update(float dt) {
+
+            if (!m_applied) {
+                apply();
+                m_applied = true;
+            }
+
+            m_elapsed += dt;
+            if (m_elapsed >= m_duration) {
+                expire();
+                m_active = false;
+            }
+        }
+
+    protected:
+        float m_elapsed;
+        float m_duration;
+        bool m_active;
+        bool m_applied;
+
+};
