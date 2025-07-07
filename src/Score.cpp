c5faebaf3cda1628ea4d157581a0e4bb49447203
darclander <lukedarclander@gmail.com>
2025-07-07 21:35:36 +0200

    minor cleanup

--- src/Score.cpp
+++ src/Score.cpp
@@ -1,20 +1,6 @@
 #include "Score.hpp"
 
-
 Score::Score(SDL_Renderer *renderer, GUI *gui, int width, int height, int type) {
-    // SDL_Surface* surface = IMG_Load("./textures/berry.png");
-    // if (!surface) {
-    //     std::cerr << "Failed to load image: " << IMG_GetError() << std::endl;
-    // }
-
-    // SDL_Texture* texture = SDL_CreateTextureFromSurface(renderer, surface);
-    // SDL_FreeSurface(surface);
-
-    // if (!texture) {
-    //     std::cerr << "Failed to create texture: " << SDL_GetError() << std::endl;
-    // }
-
-
     m_renderer = renderer;
     m_width = width;
     m_xPos = 99999;
@@ -23,7 +9,6 @@ Score::Score(SDL_Renderer *renderer, GUI *gui, int width, int height, int type) 
     m_gui = gui;
     m_type = type;
     m_textureScore = m_gui->getTexture(m_type);
-    // std::cout << m_textureScore << std::endl;
 }
 
 void Score::move(int xPos, int yPos) {
@@ -32,7 +17,7 @@ void Score::move(int xPos, int yPos) {
 }
 
 void Score::render() {
-    SDL_Rect destinationRect = {m_xPos, m_yPos,  // x, y
+    SDL_Rect destinationRect = {m_xPos, m_yPos,     // x, y
                                 m_width, m_height}; // width, height
     SDL_RenderCopy(m_renderer, m_textureScore, NULL, &destinationRect);
 }
