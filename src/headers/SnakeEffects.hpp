ae3d86efe5c1e94ceedd6d88318bbdc29404e6ad
darclander <lukedarclander@gmail.com>
2025-08-18 23:17:54 +0200

    Add additional game effects
    To make the game more interesting we are adding a couple of fun effects
    that will impact your snake!

--- src/headers/SnakeEffects.hpp
+++ src/headers/SnakeEffects.hpp
@@ -48,3 +48,69 @@ class SpeedBoostEffect : public Effect {
         Snake &m_target; // Should this be moved to another base class?
 
 };
+
+class SlowBoostEffect : public Effect {
+
+    public:
+        SlowBoostEffect(Snake &target, float duration) : Effect(duration), m_target(target) {}
+
+        void apply() override {
+            m_target.applySlowBoost();
+        }
+
+        void expire() override {
+            m_target.removeSlowBoost();
+        }
+
+        TextureID getType() override {
+            return TextureID::SLOW;
+        }
+
+    private:
+        Snake &m_target; // Should this be moved to another base class?
+
+};
+
+class GhostEffect : public Effect {
+
+    public:
+        GhostEffect(Snake &target, float duration) : Effect(duration), m_target(target) {}
+
+        void apply() override {
+            m_target.becomeGhost();
+        }
+
+        void expire() override {
+            m_target.removeGhost();
+        }
+
+        TextureID getType() override {
+            return TextureID::GHOST;
+        }
+
+    private:
+        Snake &m_target; // Should this be moved to another base class?
+
+};
+
+class FreezeEffect : public Effect {
+
+    public:
+        FreezeEffect(Snake &target, float duration) : Effect(duration), m_target(target) {}
+
+        void apply() override {
+            m_target.freeze();
+        }
+
+        void expire() override {
+            m_target.unfreeze();
+        }
+
+        TextureID getType() override {
+            return TextureID::FREEZE;
+        }
+
+    private:
+        Snake &m_target; // Should this be moved to another base class?
+
+};
